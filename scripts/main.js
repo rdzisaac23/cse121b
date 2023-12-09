ac0a6be426b254c4b44c54616ea9bb2c2384e0ee
Isaac Rodriguez <rod18056@byui.edu>
2023-12-09 13:36:06 -0700

    project6

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,23 @@ const toggleMenu = () => {
 
 document.querySelector('#toggleMenu').addEventListener('click', toggleMenu);
 
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger');
+    const aside = document.querySelector('aside');
+    const main = document.querySelector('main');
+
+    hamburger.addEventListener('click', function() {
+        // Check if aside is open
+        const isAsideOpen = aside.style.right === '0px';
+
+        if (isAsideOpen) {
+            // Close the sidebar
+            aside.style.right = '-250px';
+            main.style.marginTop = '0';
+        } else {
+            // Open the sidebar
+            aside.style.right = '0';
+            main.style.marginTop = '250px'; // Adjust based on the height of the aside
+        }
+    });
+});
